0c8adc720f93c218c3e6a6ced37c31be6e228d04
Noam Lewis <lenoam+BDB@gmail.com>
2013-07-09 11:14:15 -0400

    Fix exceptions while typing in date using keyboard input

--- src/date.js
+++ src/date.js
@@ -95,7 +95,7 @@ angular.module('ui.date', [])
           return null;
         });
         modelCtrl.$parsers.push(function(value){
-          if (value) {
+          if (angular.isDate(value)) {
             return jQuery.datepicker.formatDate(dateFormat, value);
           }
           return null;
